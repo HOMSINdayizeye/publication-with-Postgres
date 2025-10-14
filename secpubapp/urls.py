df6a6af51cf99efb21977c4e9dc4e1b0bd7ddd7d
HOMSINdayizeye <nigezamos13@gmail.com>
2025-10-14 12:04:08 +0200

    Updated edit

--- secpubapp/urls.py
+++ secpubapp/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path
-from .import views
-from django.views.generic import RedirectView
+from . import views
 
 urlpatterns = [
-path('', views.login_user, name='login'),
-path('', views.add_book, name='add_book'),
-path('add/',views.add_book, name='add_book'),
-path('', views.edit_book, name='edit_book'),
-path('', RedirectView.as_view(url='/secpubapp/', permanent=False)),
-path('books/', views.book_list, name='book_list'),
-path('delete/<int:pk>/', views.delete_book, name='delete_book'),
-path('Edit/<int:pk>/', views.edit_book, name='edit_book'),
-
+    path('', views.login_user, name='login'),
+    path('add/', views.add_book, name='add_book'),
+    path('books/', views.book_list, name='book_list'),
+    path('delete/<int:pk>/', views.delete_book, name='delete_book'),
+    path('edit/<int:pk>/', views.edit_book, name='edit_book'),
 ]
--- secpubapp/views.py
+++ secpubapp/views.py
@@ -24,16 +24,15 @@ def delete_book(request, pk):
     book.delete()
     return redirect('book_list')
 def edit_book(request, pk):
-    book= get_object_or_404(Book, pk=pk)
-    if request.method =="POST":
-     form = FormBook(request.POST, instance=book)
-    if form.is_valid():
-         form.save()
-         #books =Book.objects.all()
-         return render('book_list')
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        form = FormBook(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
     else:
-       form = FormBook(instance=Book)
-    return render(request, 'edit_book.html', {'form':form})
+        form = FormBook(instance=book)
+    return render(request, 'edit_book.html', {'form': form})
      
 def login_user(request):
     if request.method == 'POST':
